747a0b7df1310df1eb6f378267999f847aa7cd24
rjridle <ryan.ridley@okstate.edu>
2022-05-30 07:37:44 -0500

    Updated new_machine_setup.sh

--- ubuntu/new_machine_setup.sh
+++ ubuntu/new_machine_setup.sh
@@ -14,6 +14,23 @@ apt install git -y
 # Install Vim
 apt install vim -y
 
+apt install tcl-dev
+apt install tk-dev
+apt install python-tk
+apt install python3-tk
+
+# Install python3
+apt install python3 -y
+# Install pip3
+apt install python3-pip -y
+
+# Install python packages
+pip install numpy
+pip install matplotlib
+
+# Install packages for ModelSim
+apt install libx
+
 # Get the files for Vim Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -28,20 +45,21 @@ vim "+:PlugInstall" "+:qall"
 echo "[INFO] Installing packages needed for tools"
 ## Packages needed for tools
 # Magic
-sudo apt install gcc -y #gcc
-sudo apt install csh -y #csh
-sudo apt install tcsh -y #tcsh
-sudo apt install tk8.6-dev -y #Tcl/Tk
-sudo apt install libqt5opengl5-dev -y #OpenGL
+apt install gcc -y #gcc
+apt install csh -y #csh
+apt install tcsh -y #tcsh
+apt install tk8.6-dev -y #Tcl/Tk
+apt install libqt5opengl5-dev -y #OpenGL
 
 # Xschem
-sudo apt install gawk -y # Gawk
-sudo apt install libx11-dev -y # Xlib (auto installed)
-sudo apt install libxpm-dev -y # Xpm
-sudo apt install bison -y # Bison
-sudo apt install flex -y # Flex
-sudo apt install libcairo2-dev -y # Cairo
-
+apt install gawk -y # Gawk
+apt install libx11-dev -y # Xlib (auto installed)
+apt install libxpm-dev -y # Xpm
+apt install bison -y # Bison
+apt install flex -y # Flex
+apt install libcairo2-dev -y # Cairo
+apt install libgtk-3-dev -y # Gaw waveform viewer
+apt install build-essential -y # Gaw waveform viewer
 
 echo "[INFO] Cloning OpenRadHardSCL repo to home directory"
 # Clone OpenRadHardSCL repo
